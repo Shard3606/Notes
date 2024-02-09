d0c299dcc981f82beee625c2a04b374efdc2229a
MaxComp-Sci <159559709+MaxComp-Sci@users.noreply.github.com>
2024-02-09 10:46:31 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,16 @@
 import tkinter
-import customtkinter
 
 # System Settings
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("black")
+tkinter.set_appearance_mode("System")
+tkinter.set_default_color_theme("black")
 
 # App Setup/Frame
-app = customtkinter.CTk()
+app = tkinter.tk()
 app.geometry("720x480")
 app.title("Notes")
 
 # Adding UI Elements
-title = customtkinter.CTkLabel(app, text = "Welcome to Notes")
+title = tkinter.tkLabel(app, text = "Welcome to Notes")
 title.pack()
 
 # Running the App
