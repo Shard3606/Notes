1a071b1c4288b525a9a7ee48a4c103025b0308dd
Shard3606 <61795930+Shard3606@users.noreply.github.com>
2024-02-13 09:49:49 -0500

    Update Passkey.py

--- Passkey.py
+++ Passkey.py
@@ -1,26 +1,61 @@
 import time
+import mysql.connector
 Username = ""
 Password = ""
 UsernameGuess = ""
 PasswordGuess = ""
+AlreadyAccount = ""
 
 def createaccount():
   global Username
   global Password
-  Username = input("Please create a username.")
-  Password = input("Please create a password.")
+  Username = input("Please create a username, or input 'Back' to go back.")
+  if Username == "Back":
+    AlrAcc()
+  else:
+    Password = input("Please create a password, or input 'Back' to go back.")
+  if Password == "Back":
+    AlrAcc()
+  else:
+    time.sleep(3)
+    AlrAcc()
 
 def signintoaccount():
   global Username
   global Password
   global UsernameGuess
   global PasswordGuess
-  UsernameGuess = input("Please input your username.")
-  PasswordGuess = input("Please input your password.")
-  if UsernameGuess == Username and PasswordGuess == Password
-    print("Welcome...")
+  UsernameGuess = input("Please input your username, or input 'Back' to go back.")
+  if UsernameGuess == "Back":
+    AlrAcc()
+  else:
+    PasswordGuess = input("Please input your password, or input 'Back' to go back.")
+  if Password == "Back":
+    AlrAcc()
+  else:
+    if UsernameGuess == Username and PasswordGuess == Password:
+      Finish()
+      time.sleep(3)
+    else:
+      print("Incorrect username or password. Please try again.")
+      time.sleep(3)
+      signintoaccount()
+
+def AlrAcc():
+  AlreadyAccount = input("Do you already have an account? Input Y for yes and N for no.")
+  if AlreadyAccount == "Y":
+    signintoaccount()
+  elif AlreadyAccount == "N":
+    createaccount()
+  else:
+    print("Please make sure you inputted Y or N.")
     time.sleep(3)
-  else
-    print("Incorrect username or password. Please try again.")
-    time.sleep(3)
-    signintoaccount():
+    AlrAcc()
+
+def Finish():
+  print("Welcome...")
+
+if Username == "" and Password == "":
+  AlrAcc()
+else:
+  signintoaccount()
