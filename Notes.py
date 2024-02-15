99e0de24703564fdb8ead211d0a8c9e4cc79a111
Shard3606 <fernandominaya63@gmail.com>
2024-02-15 10:10:50 -0500

    h

--- Notes.py
+++ Notes.py
@@ -1,8 +1,41 @@
+import time
 Notes = []
 Selection = ""
 
-print("Welcome to your Notes!")
-Selection = input("Write 'C' if you would like to create a note," '\n'
+def CreateNote():
+    TempNoteName = input("What would you like the name of the note to be?")
+
+def EditNote():
+    global NoteList
+    print(str(Notes))
+    TempNoteName = input("What note would you like to edit?")
+
+def ViewNote():
+    global NoteList
+    print(str(Notes))
+    TempNoteName = input("What note would you like to view?")
+
+def DeleteNote():
+    global NoteList
+    print(str(Notes))
+    TempNoteName = input("What note would you like to delete?")
+
+def Welcome():
+    global Selection
+    print("Welcome to your Notes!")
+    Selection = input("Write 'C' if you would like to create a note," '\n'
                   "write 'E' if you would like to edit an existing note," '\n'
                   "write 'V' if you would like to view an existing note," '\n'
                   "write 'D' if you would like to delete an existing note.")
+    if Selection == "C":
+        CreateNote()
+    elif Selection == "E":
+        EditNote()
+    elif Selection == "V":
+        ViewNote()
+    elif Selection == "D":
+        DeleteNote()
+    else:
+        "Please make sure to input one of the available options."
+        time.sleep(3)
+        Welcome()
