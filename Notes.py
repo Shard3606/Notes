638ca94447285b2d4ed15e30b0d5cd890e6b85f8
Shard3606 <fernandominaya63@gmail.com>
2024-02-15 09:58:25 -0500

    It's all good

--- Notes.py
+++ Notes.py
@@ -1,1 +1,8 @@
+Notes = []
+Selection = ""
 
+print("Welcome to your Notes!")
+Selection = input("Write 'C' if you would like to create a note," '\n'
+                  "write 'E' if you would like to edit an existing note," '\n'
+                  "write 'V' if you would like to view an existing note," '\n'
+                  "write 'D' if you would like to delete an existing note.")
