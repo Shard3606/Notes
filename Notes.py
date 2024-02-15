c4fc0c45bf07f429d2fff0e120a74ab481a847e1
Shard3606 <fernandominaya63@gmail.com>
2024-02-15 10:00:17 -0500

    g

--- Notes.py
+++ Notes.py
@@ -5,4 +5,4 @@ print("Welcome to your Notes!")
 Selection = input("Write 'C' if you would like to create a note," '\n'
                   "write 'E' if you would like to edit an existing note," '\n'
                   "write 'V' if you would like to view an existing note," '\n'
-                  "write 'D' if you would like to delete an existing note.")
+                  "write 'D' if you would like to delete an existing note.")h
