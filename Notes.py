f1ff8053ab91e3f242a1aaaed3cea491e72ee0bd
Shard3606 <61795930+Shard3606@users.noreply.github.com>
2024-02-20 08:31:45 -0500

    Update Notes.py

--- Notes.py
+++ Notes.py
@@ -1,20 +1,87 @@
 import time
 Notes = []
 Selection = ""
+Note1 = ["NoteName", "NoteText"]
+Note2 = ["NoteName", "NoteText"]
+Note3 = ["NoteName", "NoteText"]
+Note4 = ["NoteName", "NoteText"]
+Note5 = ["NoteName", "NoteText"]
 
 def CreateNote():
-    TempNoteName = input("What would you like the name of the note to be?")
+    global Note1
+    global Note2
+    global Note3
+    global Note4
+    global Note5
+    TempNoteName = input("What would you like the name of the note to be? Or input 'Back' to go back.")
+    if TempNoteName = str("Back")
+        Welcome()
     print(str(TempNoteName))
     print('\n')
     NoteText = input("Start writing!")
+    if Note1 = ["NoteName", "NoteText"]
+        Note1[1] = str(TempNoteName)
+        Note1[2] = str(NoteText)
+        print("Saving note...")
+        time.sleep(3)
+        Welcome()
+    elif Note2 = ["NoteName", "NoteText"]
+        Note2[1] = str(TempNoteName)
+        Note2[2] = str(NoteText)
+        print("Saving note...")
+        time.sleep(3)
+        Welcome()
+    elif Note3 = ["NoteName", "NoteText"]
+        Note3[1] = str(TempNoteName)
+        Note3[2] = str(NoteText)
+        print("Saving note...")
+        time.sleep(3)
+        Welcome()
+    elif Note4 = ["NoteName", "NoteText"]
+        Note4[1] = str(TempNoteName)
+        Note4[2] = str(NoteText)
+        print("Saving note...")
+        time.sleep(3)
+        Welcome()
+    elif Note5 = ["NoteName", "NoteText"]
+        Note5[1] = str(TempNoteName)
+        Note5[2] = str(NoteText)
+        print("Saving note...")
+        time.sleep(3)
+        Welcome()
+    else:
+        print("Out of space, please delete a note to continue.")
+        time.sleep(3)
+        Welcome()
     #Save Note Text to Database
     time.sleep(3)
     Welcome()
 
 def EditNote():
     global NoteList
+    global Note1
+    global Note2
+    global Note3
+    global Note4
+    global Note5
+    Note1[1] = Note1Name
+    Note2[1] = Note2Name
+    Note3[1] = Note3Name
+    Note4[1] = Note4Name
+    Note5[1] = Note5Name
     print(str(Notes))
     TempNoteName = input("What note would you like to edit?")
+    if Note1 != ["NoteName", "NoteText"]
+        print(Note1[1])
+    if Note2 != ["NoteName", "NoteText"]
+        print(Note2[1])
+    if Note3 != ["NoteName", "NoteText"]
+        print(Note3[1])
+    if Note4 != ["NoteName", "NoteText"]
+        print(Note4[1])
+    if Note5 != ["NoteName", "NoteText"]
+        print(Note5[1])
+    print("Please input the name of the note you would like to edit, or input 'Back' to go back.")
     #Fetch Note list and note, print note, make editable
     time.sleep(3)
     Welcome()
